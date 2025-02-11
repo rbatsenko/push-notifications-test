2745536c6380d57ddd0cb99665bccf705dfca185
Roman Batsenko <rbatsenko@gmail.com>
2025-02-11 16:33:20 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,46 +1,7 @@
 import type { NextConfig } from "next";
 import withPWA from "next-pwa";
 
-const config: NextConfig = {
-  headers: async () => {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-        ],
-      },
-      {
-        source: "/sw.js",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/javascript; charset=utf-8",
-          },
-          {
-            key: "Cache-Control",
-            value: "no-cache, no-store, must-revalidate",
-          },
-          {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self'",
-          },
-        ],
-      },
-    ];
-  },
-};
+const config: NextConfig = {};
 
 const nextConfig = withPWA({
   ...config,
